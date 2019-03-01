0666edb3722cf1c1e3582037727cda61c35ea6a9
Tyler Smith <35615709+tms0050@users.noreply.github.com>
2019-02-28 23:14:32 -0600

    decision tree and calculateDecisionMatrix written. Need to test

---
+++ Dev/DecisionTreeObj.py
@@ -0,0 +1,35 @@
+'''
+Created on Feb 28, 2019
+
+@author: Tyler
+'''
+
+class DecisionTreeObj(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, objectName=None, valueMatrix=None):
+        self.leftVal = None
+        self.middleVal = None
+        self.rightVal = None
+        self.paramName = objectName
+        self.values = valueMatrix
+        '''
+        Constructor
+        '''
+    
+    def insertChild(self, objectName=None, valueMatrix=None):
+        if self.leftVal is None:
+            self.leftVal = DecisionTreeObj(objectName, valueMatrix)
+        elif self.middleVal is None:
+            self.middleVal = DecisionTreeObj(objectName, valueMatrix)
+        else:
+            self.rightVal = DecisionTreeObj(objectName, valueMatrix)
+            
+    def getName(self):
+        return self.paramName
+    
+    def getValues(self):
+        return self.values
--- Dev/decisionTree.py
+++ Dev/decisionTree.py
@@ -4,6 +4,8 @@ Created on Feb 25, 2019
 @author: Tyler
 '''
 import math
+import os
+import DecisionTreeObj
 
 def calcInfoGain(infoArray, totPos, totNeg, numAttributes = 2):
     negativeVals = 0
@@ -29,3 +31,68 @@ def calcEntropy(positives, negatives):
     negativeRatio = 1 - positiveRatio
     entropyVal = -1 * positiveRatio * math.log(positiveRatio,2) - negativeRatio * math.log(negativeRatio,2)
     return entropyVal
+
+# How to iterate through directory source:
+# https://stackoverflow.com/questions/10377998/how-can-i-iterate-over-files-in-a-given-directory
+# How to get a singular file:
+# https://stackoverflow.com/questions/13223737/how-to-read-a-file-in-other-directory-in-python
+def calculateDecisionMatrix():
+    fastaDir = os.fsencode("C:\Users\Tyler\eclipse-workspace\Comp5970P2\5970_6970_SP_19_PROJECT_2\fasta")
+    decisionMatrix = [[0 for _ in range(0, 150)] in range(0, 8)]
+    fileLoc = 0
+    saDir = os.fsencode("C:\Users\Tyler\eclipse-workspace\Comp5970P2\5970_6970_SP_19_PROJECT_2\sa")
+    for file in os.listdir(fastaDir):
+        filename = os.fsdecode(file)
+        saFilename = filename[:4] + ".sa"
+        saFile = open(saDir+saFilename, "r")
+        buriedVal = saFile.readline().count('B')
+        exposedVal = saFile.readline().count('E')
+        if buriedVal > exposedVal:
+            decisionMatrix[fileLoc][7] = 0
+        else:
+            decisionMatrix[fileLoc][7] = 1
+        optimalVal = calculateHighestOccurences(file.readline())
+        for i in range(0, 7):
+            decisionMatrix[fileLoc][i] = optimalVal[i]
+        fileLoc+=1
+    return decisionMatrix
+
+def calculateHighestOccurences(aminoList):
+    valueCounter = [[0 for _ in range(0, 3)] for _ in range(0,7)]
+    aminoProperties = {
+        'A': [1,0,0,1,1,0,0],
+        'C': [1,0,0,1,0,0,0],
+        'D': [0,1,1,1,0,0,0],
+        'E': [0,1,1,0,0,0,1],
+        'F': [1,0,0,0,0,1,1],
+        'G': [1,0,0,1,1,0,0],
+        'H': [0,1,2,0,0,1,1],
+        'I': [1,0,0,0,0,2,1],
+        'K': [0,1,2,0,0,0,1],
+        'L': [1,0,0,0,0,2,1],
+        'M': [1,0,0,0,0,0,1],
+        'N': [0,1,0,1,0,0,0],
+        'P': [1,0,0,1,0,0,0],
+        'Q': [0,1,0,0,0,0,1],
+        'R': [0,1,2,0,0,0,1],
+        'S': [0,1,0,1,1,0,0],
+        'T': [1,1,0,1,0,0,0],
+        'V': [1,0,0,1,0,2,1],
+        'W': [1,0,0,0,0,1,1],
+        'Y': [1,1,0,0,0,1,1]}
+    for i in range(0, len(aminoList)):
+        currentChar = aminoProperties[aminoList[i]]
+        for j in range(0, 7):
+            valueCounter[j][currentChar[j]]+=1
+    optimalMatrix = [0 for _ in range(0, 7)]
+    for i in range(0, 7):
+        if (valueCounter[i][2] > valueCounter[i][1]) & (valueCounter[i][2] > valueCounter[i][0]):
+            optimalMatrix[i] = 2
+        elif valueCounter[i][1] > valueCounter[i][0]:
+            optimalMatrix[i] = 1
+        else:
+            optimalMatrix[i] = 0
+    return optimalMatrix
+            
+        
+        
